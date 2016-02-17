9bc4ea9cea844608077ff56fd4eda5ea1940aacc
Paul Gomersbach <p.gomersbach@rely.nl>
2016-02-17 15:59:45 +0100

    add debian ami fix bootme

--- files/bootme.sh
+++ files/bootme.sh
@@ -55,9 +55,9 @@ fi
 
 # install puppet if not installed
 if which puppet > /dev/null 2>&1; then
-    bash /root/role_appl/files/bootstrap.sh $PUPPETMAJOR
-  else
     echo "Puppet is already installed."
+  else
+    bash /root/role_appl/files/bootstrap.sh $PUPPETMAJOR
 fi
 
 # prepare bundle

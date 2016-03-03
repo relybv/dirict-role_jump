1c6b4216334e99bdf785500cdddc39bb4a9e5fab
Paul Gomersbach <p.gomersbach@rely.nl>
2016-03-03 07:33:36 +0100

    remove rspec_monitor

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -17,7 +17,6 @@ describe 'role_appl' do
           it { is_expected.to contain_class('role_appl') }
        
           it { is_expected.to contain_class('profile_base') }
-          it { is_expected.to contain_class('profile_base::rspec_monitor') }
 
         end
       end

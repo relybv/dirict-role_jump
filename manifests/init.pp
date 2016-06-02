7a171b5ffdb2bb551dc4fa9d052f6b0e11bbcf55
Paul Gomersbach <p.gomersbach@rely.nl>
2016-06-02 13:03:14 +0000

    enable rspec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class role_appl
   # a role includes one or more profiles and at least a 'base' profile
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
-#  include ::profile_base::rspec_monitor
+  include ::profile_base::rspec_monitor
   include ::profile_apache
-#  include ::profile_apache::rspec_monitor
+  include ::profile_apache::rspec_monitor
 }

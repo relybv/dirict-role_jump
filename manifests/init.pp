3671b89dcdac1681328d2b4d6fbfadaa91c1a674
Patrick Mans <p.mans@rely.nl>
2016-07-14 11:13:20 +0200

    added profile jumphost

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,6 @@ class role_appl
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
   include ::profile_base::rspec_monitor
-  include ::profile_apache
-  include ::profile_apache::rspec_monitor
+  # include profile jumphost
+  include ::profile_jumphost
 }
